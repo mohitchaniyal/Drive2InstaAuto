1baa64272d5317534b0df593c5a55fde25d2558f
Mohit Chaniyal <61289724+mohitchaniyal@users.noreply.github.com>
2023-10-24 18:04:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ wget  https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/go
 sudo dpkg -i google-chrome-stable*
 sudo apt-get install -f
 wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/118.0.5993.70/linux64/chromedriver-linux64.zip
-unzip https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/118.0.5993.70/linux64/chromedriver-linux64.zip
+unzip chromedriver-linux64.zip
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 	
