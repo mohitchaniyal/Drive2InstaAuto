dd156b5e1f42416c2f3a47d8d7fba832c1eec451
Mohit Chaniyal <61289724+mohitchaniyal@users.noreply.github.com>
2023-10-24 17:27:10 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+wget  https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_118.0.5993.70-1_amd64.deb
+sudo dpkg -i google-chrome-stable*
+sudo apt-get install -f
+unzip https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/118.0.5993.70/linux64/chromedriver-linux64.zip
+
+	
