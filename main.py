84f8f99178b8a26e2696e6a0639ca216a60e16ad
Mohit Chaniyal <61289724+mohitchaniyal@users.noreply.github.com>
2023-10-29 14:29:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,69 +28,83 @@ class Drive2InstaAuto:
         self.__file_path="doggo.mp4"
         self.__day=datetime.today().date()-datetime.strptime(self.__day,"%d-%m-%Y").date()
         
-        
+
     def __get_file(self):
-        URL = f"https://drive.google.com/uc?id={file_id}"
-        response=requests.get(URL,stream=True)
-        with open("doggo.mp4","wb") as f:
-            f.write(response.content)
+        try:
+            URL = f"https://drive.google.com/uc?id={file_id}"
+            response=requests.get(URL,stream=True)
+            with open("doggo.mp4","wb") as f:
+                f.write(response.content)
+        except Exception as e:
+            print(e)
 
     def __initialize_browser(self):
-        url='https://www.instagram.com/'
-        driver_path = 'chromedriver-linux64/chromedriver'
-        chrome_options = ChromeOptions()
-        chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--no-sandbox")
-        # options = [ "--ignore-certificate-errors",
-        #         	"--headless",
-        #             "--disable-gpu",
-        #             "--window-size=1920,1200",
-        #             "--disable-extensions",
-        #             "--no-sandbox",
-        #             "--disable-dev-shm-usage"]
-        #             #'--remote-debugging-port=9222']
-        # for option in options:
-        #     chrome_options.add_argument(option)
-        browser = Chrome(service=Service(driver_path),options=chrome_options)
-        browser.implicitly_wait(5)
-        browser.get(url)
-        browser.maximize_window()
-        self.__browser=browser
-        return {"success":True}
+        try:
+            url='https://www.instagram.com/'
+            driver_path = 'chromedriver-linux64/chromedriver'
+            chrome_options = ChromeOptions()
+            chrome_options.add_argument("--window-size=1920,1080")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-gpu")
+            chrome_options.add_argument("--no-sandbox")
+            # options = [ "--ignore-certificate-errors",
+            #         	"--headless",
+            #             "--disable-gpu",
+            #             "--window-size=1920,1200",
+            #             "--disable-extensions",
+            #             "--no-sandbox",
+            #             "--disable-dev-shm-usage"]
+            #             #'--remote-debugging-port=9222']
+            # for option in options:
+            #     chrome_options.add_argument(option)
+            browser = Chrome(service=Service(driver_path),options=chrome_options)
+            browser.implicitly_wait(5)
+            browser.get(url)
+            browser.maximize_window()
+            self.__browser=browser
+            return {"success":True}
+        except Exception as e:
+            print(e)
     
     def __login(self):
-        
-        with open("insta.json", 'r') as cookiesfile:
-            cookies = json.load(cookiesfile)
-        for cookie in cookies:
-            self.__browser.add_cookie(cookie)
-        self.__browser.get("https://www.instagram.com/")
-        time.sleep(2)
-        self.__browser.find_element(By.XPATH,"//button[text()='Not Now']").click()
-        return {"success":True}
+        try:
+            with open("insta.json", 'r') as cookiesfile:
+                cookies = json.load(cookiesfile)
+            for cookie in cookies:
+                self.__browser.add_cookie(cookie)
+            self.__browser.get("https://www.instagram.com/")
+            time.sleep(5)
+            self.__browser.find_element(By.XPATH,"//button[text()='Not Now']").click()
+            return {"success":True}
+        except Exception as e:
+            self.__browser.save_screenshot("screenshot.png")
+            print(e)
     
     def __upload_post(self):
-        wait_time = 10
-        time.sleep(5)
-        self.__browser.find_element(By.XPATH,"//span[text()='Create']").click()
-        time.sleep(1)
-        element=self.__browser.find_element(By.XPATH,"//span[text()='Create']")
-        
-        driver = element.parent
-        file_input = driver.execute_script(self.__JS_DROP_FILE,element, 0, 0)
-        time.sleep(5)
-        file_input.send_keys(self.__file_path)
-        time.sleep(5)
-        self.__browser.find_element(By.XPATH,"//button[text()='OK']").click()
-        self.__browser.find_element(By.XPATH,"//div[text()='Next']").click()
-        self.__browser.find_element(By.XPATH,"//div[text()='Next']").click()
-        self.__browser.find_element(By.XPATH,"//div[text()='Write a caption...']/..//p").send_keys(f"Day {self.__day} \n #dogs")
-        self.__browser.find_element(By.XPATH,"//div[text()='Share']").click()
-        time.sleep(10)
+        try:
+            wait_time = 10
+            time.sleep(5)
+            self.__browser.find_element(By.XPATH,"//span[text()='Create']").click()
+            time.sleep(1)
+            element=self.__browser.find_element(By.XPATH,"//span[text()='Create']")
+            
+            driver = element.parent
+            file_input = driver.execute_script(self.__JS_DROP_FILE,element, 0, 0)
+            time.sleep(5)
+            file_input.send_keys(self.__file_path)
+            time.sleep(5)
+            self.__browser.find_element(By.XPATH,"//button[text()='OK']").click()
+            self.__browser.find_element(By.XPATH,"//div[text()='Next']").click()
+            self.__browser.find_element(By.XPATH,"//div[text()='Next']").click()
+            self.__browser.find_element(By.XPATH,"//div[text()='Write a caption...']/..//p").send_keys(f"Day {self.__day} \n #dogs")
+            self.__browser.find_element(By.XPATH,"//div[text()='Share']").click()
+            time.sleep(10)
 #         self.__browser.save_screenshot("test.png")
-        return {"success":True}
+            return {"success":True}
+        except Exception as e:
+            self.__browser.save_screenshot("screenshot.png")
+            print(e)
+        
     
     def _start_uploding_file(self):
         self.__initialize_browser()
